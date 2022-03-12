9c5e95bbb1ec75db15625f0d802b092874ba7e84
dkgusdkfk <dkgusdkfk@gmail.com>
2022-03-12 23:55:55 +0900

    그리드 숫자카드게임 문제 코드 작성

--- 이코테/Chapter3 그리디/숫자 카드 게임.py
+++ 이코테/Chapter3 그리디/숫자 카드 게임.py
@@ -1,1 +1,10 @@
+#N, M 입력
+N, M = map(int, input().split())
 
+min_list = [] #각 행의 가장 작은 수를 넣을 리스트
+for i in range(N):
+    min_list.append(min(list(map(int, input().split())))) #각 행을 입력받아 각 행에서 가장 작은 수를 min_list에 append
+
+result = max(min_list) # 각 행에서 가장 작은 수 중 가장 큰 수 선택
+
+print(result) #결과 값 출력
