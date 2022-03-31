8a7e6792c983e00d62d0ed0852b7a6c526de92ca
dkgusdkfk <dkgusdkfk@gmail.com>
2022-04-01 00:02:44 +0900

    chapter8 효율적인화폐구성 코드 작성

--- 이코테/Chapter8 다이나믹 프로그래밍/효율적인화폐구성.py
+++ 이코테/Chapter8 다이나믹 프로그래밍/효율적인화폐구성.py
@@ -6,3 +6,20 @@ array = []
 for i in range(N):
     array.append(int(input()))
 
+# DP 테이블 초기화
+d = [0] * 100001
+
+
+for i in array:
+    d[i] = 1
+
+for i in range(0, min(array)):
+    d[i] = -1
+
+for i in range(min(array)+1, M+1):
+    if i-max(array) > 0 and i not in array:
+        d[i] = min([d[i-j]+1 for j in array if d[i-j] != -1])
+    else:
+        d[i] = -1
+
+print(d[M])
