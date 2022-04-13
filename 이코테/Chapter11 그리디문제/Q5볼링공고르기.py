274edcc3540604c93934d6d5bb79dba87e685950
dkgusdkfk <dkgusdkfk@gmail.com>
2022-04-13 11:01:15 +0900

    chapter11 Q5볼링공고르기 코드작성

---
+++ 이코테/Chapter11 그리디문제/Q5볼링공고르기.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+balls = list(map(int, input().split()))
+
+balls.sort()
+
+result = 0
+for i in range(n-1):
+    for j in range(i+1,n):
+        if balls[i] != balls[j]:
+            result += 1
+
+print(result)
