7a8e26b6976d486bb0e2e669a7605d2c0cb7466f
dkgusdkfk <dkgusdkfk@gmail.com>
2022-04-13 10:34:16 +0900

    chapter11 Q2곱하기혹은더하기 코드 수정

--- 이코테/Chapter11 그리디문제/Q2곱하기혹은더하기.py
+++ 이코테/Chapter11 그리디문제/Q2곱하기혹은더하기.py
@@ -1,12 +1,8 @@
-s = map(int, input())
+s = list(map(int, input()))
 
-num = []
-for i in s:
-    num.append(int(i))
+result = s[0]
 
-result = num[0]
-
-for i in num[1:]:
+for i in s[1:]:
     if result == 0 or i == 0:
         result += i
     else:
