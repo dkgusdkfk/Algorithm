e89fd933fc1af491bd8470ef2958971379d471a1
dkgusdkfk <dkgusdkfk@gmail.com>
2022-04-30 23:31:13 +0900

    백준 Q21608 코드 작성

--- 백준/Q21608.py
+++ 백준/Q21608.py
@@ -1,25 +1,24 @@
-from collections import Counter
-
 def possible_seat(seat, x, y, f):
     slist = []
     dx = [-1, 0, 0, 1]
     dy = [0, -1, 1, 0]
     for i in range(4):
-        if 0 <= x+dx[i] < n and 0 <= y+dy[i] < n:
-            if seat[x+dx[i]][y+dy[i]] == f:
-                slist.append((x+dx[i], y+dy[i]))
+        if 0 <= x + dx[i] < n and 0 <= y + dy[i] < n:
+            if seat[x + dx[i]][y + dy[i]] == f:
+                slist.append((x + dx[i], y + dy[i]))
     return slist
+
 
 n = int(input())
 
-seat =[]
+seat = []
 for i in range(n):
-    seat.append([0]*n)
+    seat.append([0] * n)
 
 dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
 dic = {}
-for i in range(n**2):
+for i in range(n ** 2):
     array = list(map(int, input().split()))
     student = array[0]
     array = array[1:]
@@ -29,30 +28,33 @@ for i in range(n**2):
         for row in range(n):
             for col in range(n):
                 if seat[row][col] == s:
-                    # one_list = possible_seat(seat, row, col)
                     for i in range(4):
                         if 0 <= row + dx[i] < n and 0 <= col + dy[i] < n:
                             if seat[row + dx[i]][col + dy[i]] == 0:
                                 one_list.append((row + dx[i], col + dy[i]))
+    one_list.sort()
     if len(one_list) > 1:
-        one = Counter(one_list).most_common(1)
-        if one[0][1] > 1:
-            result = (one[0][0][0], one[0][0][1])
-        else:
-            count = 0
-            for x, y in one_list:
-                if count < len(possible_seat(seat, x, y, 0)):
-                    count = len(possible_seat(seat, x, y, 0))
-                    result = (x,y)
+        o_count = 0
+        for o in one_list:
+            if o_count < one_list.count(o):
+                max_list = [o]
+                o_count = one_list.count(o)
+            elif o_count == one_list.count(o):
+                max_list.append(o)
+        count = -1
+        for x, y in max_list:
+            if count < len(possible_seat(seat, x, y, 0)):
+                count = len(possible_seat(seat, x, y, 0))
+                result = (x, y)
     elif len(one_list) == 1:
         result = one_list[0]
     else:
-        count = 0
+        count = -1
         for i in range(n):
             for j in range(n):
-                if count < len(possible_seat(seat, i, j, 0)):
+                if seat[i][j] == 0 and count < len(possible_seat(seat, i, j, 0)):
                     count = len(possible_seat(seat, i, j, 0))
-                    result = (i,j)
+                    result = (i, j)
 
     seat[result[0]][result[1]] = student
 
@@ -63,6 +65,6 @@ for i in range(n):
         for v in dic[seat[i][j]]:
             c += len(possible_seat(seat, i, j, v))
         if c > 0:
-            sum += 10**(c-1)
+            sum += 10 ** (c - 1)
 
 print(sum)
