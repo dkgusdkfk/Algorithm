af74064ac63ea509c77cef791ac46520b809619b
dkgusdkfk <dkgusdkfk@gmail.com>
2022-07-07 23:43:45 +0900

    Q29 공유기 설치 코드 작성

---
+++ 이코테/Chapter15 이진탐색문제/Q29공유기설치.py
@@ -0,0 +1,27 @@
+n, c = map(int, input().split())
+
+a = []
+for _ in range(n):
+    a.append(int(input()))
+
+a.sort()
+
+start = 1
+end = a[-1] - a[0]
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 1
+    value = a[0]
+    for i in range(1, n):
+        if a[i] >= value + mid:
+            count += 1
+            value = a[i]
+    if count >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)
