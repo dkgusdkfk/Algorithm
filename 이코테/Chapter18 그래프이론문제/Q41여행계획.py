9582395c2eb796fc515c6168fa2796bdabfb641b
dkgusdkfk <dkgusdkfk@gmail.com>
2022-07-14 23:44:16 +0900

    .

--- 이코테/Chapter18 그래프이론문제/Q41여행계획.py
+++ 이코테/Chapter18 그래프이론문제/Q41여행계획.py
@@ -1,9 +1,10 @@
 import sys
 
-n, m = int(input().split)
+n, m = int(input().split())
 
 graph = []
 
 for i in range(n):
     graph.append(list(map(int, sys.stdin.readline().split())))
 
+plan = list(map(int, input().split()))
