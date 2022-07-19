751d5d7b8c3ff1e36a12b2c2680f4241d6291da8
dkgusdkfk <dkgusdkfk@gmail.com>
2022-07-20 00:32:25 +0900

    Q41 여행계획 코드작성

--- 이코테/Chapter18 그래프이론문제/Q41여행계획.py
+++ 이코테/Chapter18 그래프이론문제/Q41여행계획.py
@@ -1,11 +1,39 @@
 import sys
 
-n, m = int(input().split())
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-graph = []
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
-for i in range(n):
-    graph.append(list(map(int, sys.stdin.readline().split())))
+n, m = map(int, input().split())
+
+parent = [0] * (n+1)
+
+for i in range(1, n+1):
+    parent[i] = i
+
+for i in range(1, n+1):
+    array = list(map(int, sys.stdin.readline().split()))[i:]
+    k = i + 1
+    for a in array:
+        if a == 1:
+            union_parent(parent, i, k)
+        k += 1
 
 plan = list(map(int, input().split()))
 
+result = 'YES'
+for i in range(len(plan)-1):
+    if find_parent(parent, plan[i]) != find_parent(parent, plan[i+1]):
+        result = 'NO'
+        break
+
+print(result)
