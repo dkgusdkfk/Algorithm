4498fd026bdbfe8770dd53eba4bc6737ac7ca8c9
dkgusdkfk <dkgusdkfk@gmail.com>
2022-07-29 00:05:45 +0900

    1이 될 때까지 코드작성

--- 이코테/Chapter3 그리디/1이 될 때까지.py
+++ 이코테/Chapter3 그리디/1이 될 때까지.py
@@ -12,3 +12,20 @@ while N != 1:
         count += 1
 
 print(count) # 결과 값 출력
+
+"""
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    target = (n//k) * k
+    result += (n-target)
+    n = target
+    if n < k:
+        break
+    result += 1
+    n //= k
+    
+result += (n-1)
+print(result)
+"""
