b0afce394f9e3266108d0665c03e6af472867774
dkgusdkfk <dkgusdkfk@gmail.com>
2022-11-20 01:51:24 +0900

    swea 1974.스도쿠검증 코드작성

---
+++ sw/2007.패턴마디의길이.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+
+for t in range(1, T+1):
+    result = 0
+
+    s = input()
+
+    for i in range(1,30):
+        if s[i] == s[0]:
+            if s[:i] == s[i:i*2]:
+                result = i
+                break
+
+    print("#%d %d" % (t, result))
