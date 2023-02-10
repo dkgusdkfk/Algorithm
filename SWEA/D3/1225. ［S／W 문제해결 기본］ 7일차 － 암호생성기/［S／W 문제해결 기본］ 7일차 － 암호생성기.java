39f42651b8cd2ab9da69ae76c60657e8ade3c4db
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-02-10 15:03:10 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 133 ms, Memory: 20,844 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+
+		for (int tc = 0; tc < 10; tc++) {
+			Queue<Integer> queue = new LinkedList<>();
+			int q = Integer.MAX_VALUE;
+			int t = sc.nextInt();
+			for (int i = 0; i < 8; i++) {
+				int k = sc.nextInt();
+				queue.offer(k);
+				q = Math.min(q, k / 15);
+			}
+			for (int i = 0; i < 8; i++) {
+				queue.offer(queue.poll() % (q - 1));
+			}
+
+			int flag = 0;
+			while (flag != 1) {
+				for (int i = 1; i <= 5; i++) {
+					int k = queue.poll();
+					if (k - i <= 0) {
+						queue.offer(0);
+						flag = 1;
+						break;
+					} else {
+						queue.offer(k - i);
+					}
+				}
+			}
+
+			sb.append("#" + t + " ");
+			while (queue.size() != 0) {
+				sb.append(queue.poll() + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+
+	}
+}
