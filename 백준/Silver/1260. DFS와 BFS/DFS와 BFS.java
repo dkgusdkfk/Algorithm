a117a84cf6e851b600834dd69601433634b254da
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-02-15 12:38:08 +0900

    [Silver II] Title: DFS와 BFS, Time: 252 ms, Memory: 23252 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int n;
@@ -8,18 +11,20 @@ public class Main {
 	static int[][] graph;
 	static StringBuilder sb;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		sb = new StringBuilder();
 		
-		n = sc.nextInt();
-		int m = sc.nextInt();
-		int v = sc.nextInt();
+		n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
 		
 		graph = new int[n+1][n+1];
 		for (int i = 0; i < m; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
+			st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 			graph[x][y] = 1;
 			graph[y][x] = 1;
 		}
