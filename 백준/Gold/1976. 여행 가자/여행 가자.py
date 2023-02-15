ec8ee2422f27ba1d40cc36d0f8ac999d5c19f1c6
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-02-15 10:03:29 +0900

    [Gold IV] Title: 여행 가자, Time: 528 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -7,9 +7,8 @@ n = int(input())
 m = int(input())
 
 graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
 for i in range(n):
+    graph.append(list(map(int, input().split())))
     graph[i][i] = 1
 
 course = list(map(int, input().split()))
