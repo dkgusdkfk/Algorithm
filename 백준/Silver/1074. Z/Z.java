1f3e8caffed2e9c6f323e579759767e410a9c9ae
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-02-20 16:04:37 +0900

    [Silver I] Title: Z, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	private static int r, c, N;
+	private static int cnt = 0;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		r = sc.nextInt();
+		c = sc.nextInt();
+		solution(0, 0, 2 << N - 1);
+
+		System.out.println(cnt);
+
+	}
+
+	private static void solution(int x, int y, int n) {
+		if (x == r && y == c)
+			return;
+		int half = n / 2;
+		if ((x + half) <= r && r < (x + n)) {
+			x += half;
+			cnt += half * half * 2;
+		}
+		if ((y + half) <= c && c < (y + n)) {
+			y += half;
+			cnt += half * half;
+		}
+		solution(x, y, half);
+	}
+}
