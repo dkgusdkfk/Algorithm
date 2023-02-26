b71b50368244a973b15a5f37a3b6106cdb1453e4
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-02-26 09:35:43 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,30 +1,23 @@
-answer = 1e9
+from collections import deque
+
 def solution(begin, target, words):
-    isSelected = [False] * len(words)
-    sol(begin, target, words, isSelected, 0)
-    if answer == 1e9:
+    if target not in words:
         return 0
-    return answer
-
-
-def sol(begin, target, words, isSelected, cnt):
-    global answer
-    if begin == target:
-        answer = min(answer, cnt)
-        return
-    for i, w in enumerate(words):
-        if isSelected[i]: continue
-        if check(begin, w):
-            isSelected[i] = True
-            sol(w, target, words, isSelected, cnt + 1)
-            isSelected[i] = False
+    
+    queue = deque()
+    queue.append([begin, 0])
+    
+    while queue:
+        word, cnt = queue.popleft()
+        
+        if word == target:
+            return cnt
+        
+        for wd in words:
+            flag = 0
+            for i, w in enumerate(word):
+                if wd[i] != w:
+                    flag += 1
+            if flag == 1:
+                queue.append([wd, cnt + 1])
     return 0
-
-def check(w1, w2):
-    flag = 0
-    for i in range(len(w1)):
-        if w1[i] != w2[i]:
-            flag += 1
-        if flag == 2:
-            return False
-    return True
