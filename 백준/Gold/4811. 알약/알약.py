92c80058712cfae5df625b317e65e6c513b52af0
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-04-14 09:40:44 +0900

    [Gold V] Title: 알약, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.py
+++ 백준/Gold/4811. 알약/알약.py
@@ -1,13 +1,13 @@
-N = int(input())
+dp = [[0] * 31 for _ in range(31)]
 
-while N != 0:
-    dp = [[0] * (N+1) for _ in range(N+1)]
+for i in range(31):
+    dp[0][i] = 1
+for i in range(1, 31):
+    for j in range(i, 31):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-    for i in range(N+1):
-        dp[0][i] = 1
-    for i in range(1, N+1):
-        for j in range(i, N+1):
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
-
-    print(dp[N][N])
+while True:
     N = int(input())
+    if N == 0:
+        break
+    print(dp[N][N])
