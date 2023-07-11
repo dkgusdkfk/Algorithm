b84d2b87e047165d43f138ac33b93b523f44079a
dkgusdkfk <dkgusdkfk@gmail.com>
2023-07-11 23:33:35 +0900

    백준 Q1926

--- 백준/Q1926.py
+++ 백준/Q1926.py
@@ -1,19 +1,28 @@
+from collections import deque
+
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
 
-def dfs(x, y, c):
-    print(x, y)
-    if graph[x][y] != 1:    return c
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
     graph[x][y] = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if isValid(nx, ny):
-            c = dfs(nx, ny, c+1)
-    return c
+    count = 1
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if isValid(nx, ny):
+                if graph[nx][ny] == 1:
+                    q.append((nx, ny))
+                    graph[nx][ny] = 0
+                    count += 1
+    return count
+
 
 def isValid(x, y):
     if 0 <= x < n and 0 <= y < m:
@@ -27,8 +36,7 @@ def main():
         for j in range(m):
             if graph[i][j] == 1:
                 total += 1
-                print(dfs(i, j, 0))
-                maxValue = max(maxValue, dfs(i, j, 0))
+                maxValue = max(maxValue, bfs(i, j))
     print(total)
     print(maxValue)
 
