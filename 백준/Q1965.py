29b0b7a98aaf9cc38a40451e26c04f89914b57b4
dkgusdkfk <dkgusdkfk@gmail.com>
2023-07-16 22:14:32 +0900

    백준 Q1965(미완성)

--- 백준/Q1965.py
+++ 백준/Q1965.py
@@ -1,2 +1,11 @@
 n = int(input())
 boxes = list(map(int, input().split()))
+dp = [0 for _ in range(n)]
+dp[0] = boxes[0]
+for i in range(1, n):
+    for j in range(1, i+1):
+        if boxes[i-j] < boxes[i]:
+            if dp[i] > dp[i-j] + 1:
+                break
+            dp[i] = dp[i-j] + 1
+print(dp[-1])
