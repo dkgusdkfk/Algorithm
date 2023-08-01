1d03f7880d7d21801fc421ae9c9b0bb5e2a13527
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-08-01 12:15:46 +0900

    프로세서연결하기 (미완성)

--- sw/B/프로세서연결하기.java
+++ sw/B/프로세서연결하기.java
@@ -1,8 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 프로세서연결하기 {
+	
+	static int[] dx = {-1, 0, 0, 1};
+	static int[] dy = {0, -1, 1, 0};
+	
+	static int result = Integer.MAX_VALUE;
+	static int coreCount = 0;
+
+	static List<Core> coreList;
+	
+	static class Core {
+		int x, y;
+		public Core(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -17,12 +35,35 @@ public class 프로세서연결하기 {
 				for (int j = 1; j <= n; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 					if (map[i][j] == 1) {
-						
+						if (i == 1 || j == 1 || i == n || j == n)	continue;
+						coreList.add(new Core(i, j));
 					}
 				}
 			}
 			
 			
+			
 		}
 	}
+	
+	private static void go(int[][] map, int idx, int count, int coreCnt) {
+		if (idx == coreList.size()) {
+			if (coreCount == coreCnt) {
+				result = Math.min(result, count);
+			} else if (coreCount < coreCnt) {
+				result = count;
+				coreCount = coreCnt;
+			}
+		}
+		
+		
+		
+	}
+	
+	private static void move(int[][] map, int x, int y, int d) {
+		
+		
+		
+	}
+	
 }
