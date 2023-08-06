8dfff12fd7f19ce6ac495966e1fafe1ea0eb8aef
dkgusdkfk <dkgusdkfk@gmail.com>
2023-08-06 22:08:01 +0900

    백준 Q15988.py

---
+++ 백준/Q15988.py
@@ -0,0 +1,13 @@
+MAX_VALUE = 1_000_000
+dp = [0 for _ in range(MAX_VALUE + 1)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, MAX_VALUE + 1):
+    dp[i] = (dp[i-1] +dp[i-2] + dp[i-3]) % 1_000_000_009
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(dp[n])
