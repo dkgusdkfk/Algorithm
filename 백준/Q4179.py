10074f6481dfc5573a9c6989e2b1850416d68446
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-08-09 19:59:37 +0900

    boj Q4179 (미완성)

--- 백준/Q4179.py
+++ 백준/Q4179.py
@@ -2,18 +2,52 @@ from collections import deque
 R, C = map(int, input().split())
 
 graph = []
-queue = deque()
-jihoon = (0, 0)
+fire = deque()
+jihoon = deque()
 for r in range(R):
-    g = []
+    g = list(input())
     for c in range(C):
-        g.append(input())
         if g[c] == 'J':
-            jihoon = ('J', r, c)
+            jihoon.append((r, c))
+            g[c] = '.'
         elif g[c] == 'F':
-            queue.append(('F', r, c))
+            fire.append((r, c))
     graph.append(g)
-queue.append(jihoon)
+visited = [[False for _ in range(C)] for _ in range(R)]
 
-while queue:
-    pass
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
+
+def spread():
+    fSize = len(fire)
+    while fSize > 0:
+        x, y = fire.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C:
+                if graph[nx][ny] == '.':
+                    graph[nx][ny] = 'F'
+                    fire.append((nx, ny))
+        fSize -= 1
+
+t = 0
+while jihoon:
+    t += 1
+    spread()
+    jSize = len(jihoon)
+    while jSize > 0:
+        x, y = jihoon.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C:
+                if graph[nx][ny] == '.' and not visited[nx][ny]:
+                    jihoon.append((nx, ny))
+                    visited[nx][ny] = True
+            else:
+                print(t)
+                exit(1)
+        jSize -= 1
+
+print("IMPOSSIBLE")
