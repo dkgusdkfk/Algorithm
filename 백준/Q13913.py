8632bbe8698245a717f0407d9c630cc2548d8167
dkgusdkfk <dkgusdkfk@gmail.com>
2023-08-19 23:29:22 +0900

    백준 Q13913.py

--- 백준/Q13913.py
+++ 백준/Q13913.py
@@ -4,7 +4,7 @@ MAX_VALUE = 100000
 
 n, k = map(int, input().split())
 queue = deque()
-visited = [0 for _ in range(MAX_VALUE+1)]
+visited = [-1 for _ in range(MAX_VALUE+1)]
 count = [0 for _ in range(MAX_VALUE+1)]
 queue.append(n)
 visited[n] = n
@@ -14,22 +14,22 @@ while queue:
     if x == k:
         print(count[k])
         break
-    if x - 1 >= 0 and visited[x-1] == 0:
+    if x - 1 >= 0 and visited[x-1] == -1:
         queue.append(x-1)
         visited[x-1] = x
         count[x-1] = count[x] + 1
-    if x + 1 <= MAX_VALUE and visited[x+1] == 0:
+    if x + 1 <= MAX_VALUE and visited[x+1] == -1:
         queue.append(x+1)
         visited[x + 1] = x
         count[x+1] = count[x] + 1
-    if x*2 <= MAX_VALUE and visited[x*2] == 0:
+    if x*2 <= MAX_VALUE and visited[x*2] == -1:
         queue.append(x*2)
         visited[x*2] = x
         count[x*2] = count[x] + 1
 
 arr = []
 while n != k:
-    arr.insert(0, k)
+    arr.append(k)
     k = visited[k]
-arr.insert(0, n)
-print(*arr)
+arr.append(n)
+print(*arr[::-1])
