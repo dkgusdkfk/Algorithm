2cf6922cf10af075b5fb5ec8b998724e89c841ab
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-08-22 23:51:38 +0900

    BOJ Q2133.py(미완성)

---
+++ 백준/Q2133.py
@@ -0,0 +1,14 @@
+n = int(input())
+if n % 2 != 0:
+    print(0)
+else:
+    dp = [0] * (n+1)
+    dp[0] = 1
+    if n >= 2:
+        dp[2] = 3
+    if n > 2:
+        temp = dp[0] + dp[2]
+        for i in range(4, n+1, 2):
+            dp[i] = dp[i-2] * 3 + temp * 2
+            temp += dp[i]
+    print(dp[n])
