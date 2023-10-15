09eb6a30c2ae2297513a802446f973e112a1f75d
dkgusdkfk <dkgusdkfk@gmail.com>
2023-10-15 22:47:37 +0900

    BOJ Q1593.py

---
+++ 백준/Q1593.py
@@ -0,0 +1,21 @@
+g, s = map(int, input().split())
+W = input()
+S = input()
+
+a = [0 for _ in range(ord('z'))]
+b = [0 for _ in range(ord('z'))]
+
+for i in range(g):
+    a[ord(W[i]) - 65] += 1
+    b[ord(S[i]) - 65] += 1
+
+count = 0
+if a == b:
+    count += 1
+for i in range(s-g):
+    b[ord(S[i]) - 65] -= 1
+    b[ord(S[i+g]) - 65] += 1
+    if a == b:
+        count += 1
+print(count)
+
