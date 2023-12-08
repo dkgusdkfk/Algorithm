a83496fceac09852db08a4e02b2b74f5c61a1d12
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-12-09 01:03:32 +0900

    [Gold IV] Title: 문자열 폭발, Time: 368 ms, Memory: 49136 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,11 +1,12 @@
 S = list(input())
 s = list(input())
 l = len(s)
+temp = s[-1]
 
 stack = []
 for i in S:
     stack.append(i)
-    if stack[-l:] == s:
+    if stack[-1] == temp and stack[-l:] == s:
         for j in range(l):
             stack.pop()
 
