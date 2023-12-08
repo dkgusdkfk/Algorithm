98f85eadf14b6bdb78091aa4938a30d7ac8c6a31
dkgusdkfk <101117330+dkgusdkfk@users.noreply.github.com>
2023-12-09 01:01:40 +0900

    [Gold IV] Title: 문자열 폭발, Time: 424 ms, Memory: 49136 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -2,16 +2,11 @@ S = list(input())
 s = list(input())
 l = len(s)
 
-stack = S[:l]
-if stack == s:
-    stack = []
-for i in S[l:]:
+stack = []
+for i in S:
     stack.append(i)
     if stack[-l:] == s:
         for j in range(l):
             stack.pop()
 
-if not stack:
-    print("FRULA")
-else:
-    print(*stack, sep="")
+print(''.join(stack) if stack else 'FRULA')
